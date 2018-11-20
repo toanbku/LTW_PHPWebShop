53726aa1bcc8da042caea3f63ad8446b107cbd94
Quang Toan <toanbku@gmail.com>
2018-11-20 16:21:20 +0700

    first version

---
+++ s-config.php
@@ -0,0 +1,12 @@
+<?php
+$dbhost = 'localhost';
+$dbuserName = 'root';
+$dbpassword = '';
+$dbName = 'assignment2';
+
+$conn = new mysqli($dbhost, $dbuserName, $dbpassword, $dbName);
+
+if (!$conn) {
+	die('Could not connect: ' . mysql_error());
+}
+?>
